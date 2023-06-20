0755f2cb2a077cdc02b7110c6074eeff46e233d9
Ioannis <jondan97@gmail.com>
2023-06-20 10:20:53 +0300

    added java recap module

---
+++ java-path-core-recap/src/main/java/gr/codelearn/Main.java
@@ -0,0 +1,17 @@
+package gr.codelearn;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.err.println("test");
+        logger.info("123");
+        logger.debug("test123");
+        logger.error("error critical");
+    }
+}
